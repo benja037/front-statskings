a69ee07eb70d081fa69b040712a0eff81ccfdad1
benjamin <benja037@gmail.com>
2024-07-13 18:25:34 -0400

    Change auth return api from api

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,19 +1,19 @@
 import Cookies from 'js-cookie';
-import api from './fetcher';
+import api from './api';
 
 export const AuthActions = {
   login: async (email: string, password: string) => {
     const { data } = await api.post('/authenticate/jwt/create', { username: email, password });
-    Cookies.set('token', data.access);
-    Cookies.set('userEmail', email);
+    Cookies.set('token', data.access, { sameSite: 'Lax' });
+    Cookies.set('userEmail', email, { sameSite: 'Lax' });
   },
   logout: async () => {
     const refreshToken = Cookies.get('refreshToken');
     if (refreshToken) {
       await api.post('/authenticate/logout/', { refresh: refreshToken });
     }
-    Cookies.remove('token');
-    Cookies.remove('refreshToken');
-    Cookies.remove('userEmail');
+    Cookies.remove('token', { sameSite: 'Lax' });
+    Cookies.remove('refreshToken', { sameSite: 'Lax' });
+    Cookies.remove('userEmail', { sameSite: 'Lax' });
   }
 };
